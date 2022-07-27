2b6e360b2837f5bdfba33347bee35e1a6defae91
jnbhm <hello@jeanboehm.de>
2022-07-27 16:03:47 +0200

    simplified button

--- src/components/button.ts
+++ src/components/button.ts
@@ -3,14 +3,9 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'KiteggButton',
   props: ['label'],
-  data() {
-    return {
-      count: 1,
-    };
-  },
   template: `
-    <button @click="count++" ref="sampleElement">
-      {{ label }} = {{count}}
+    <button>
+      {{ label }}
     </button>
   `,
 });
