b57ea3fd93b0f65133f4222f4fff5254cd6b00fc
jnbhm <hello@jeanboehm.de>
2022-07-27 14:25:13 +0200

    removed mounted

--- src/components/button.ts
+++ src/components/button.ts
@@ -8,20 +8,6 @@ export default defineComponent({
       count: 1,
     };
   },
-  mounted() {
-    console.log('Component mounted');
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    console.log(this.count);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    console.log(this.$refs.sampleElement);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    this.$nextTick(() => {
-      console.log('NEXT TICK');
-    });
-  },
   template: `
     <button @click="count++" ref="sampleElement">
       {{ label }} = {{count}}
