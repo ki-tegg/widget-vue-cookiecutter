8546979ffd86d76784fa33908365916542437233
Francesco Scheffczyk <francesco.scheffczyk@students.hs-mainz.de>
2022-09-27 10:43:44 +0200

    Refactor code base structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Jean Boehm
 // Distributed under the terms of the Modified BSD License.
 
-export * from './version';
 export * from './widget';
+
+export * from './version';
---
+++ src/widget/Example.vue
@@ -0,0 +1,44 @@
+<template>
+  <input
+    class="q-mr-lg"
+    type="text"
+    ref="exampleInput"
+    :placeholder="data.value"
+  />
+  <KBtn
+    label="Hallo"
+    size="sm"
+    :primary="false"
+    :dark="true"
+    @click="changeStoreModel"
+  />
+  <div>Neue Changes :)!</div>
+  <KSelect :options="['mail', 'Figma', 'Mattermost']" label="Contact" dense />
+</template>
+
+<script setup lang="ts"></script>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { useExampleStore } from './store';
+import { mapState, mapActions } from 'pinia';
+
+export default defineComponent({
+  computed: {
+    ...mapState(useExampleStore, ['data']),
+  },
+  methods: {
+    ...mapActions(useExampleStore, {
+      setData: 'setData',
+    }),
+    changeStoreModel() {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      this.setData('value', this.$refs.exampleInput.value);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
