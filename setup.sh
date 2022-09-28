07bc12e72eb1b34979e604b3a105e36ebaa07e0c
Jean Böhm <jean.boehm@hs-mainz.de>
2022-09-28 11:12:05 +0200

    Fix sfc

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-jlpm add --save @kitegg/kitegg-ui
+npm install --save @kitegg/kitegg-ui
+jlpm install
 
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade setuptools
+jlpm run build
+
 python3 -m pip install --no-cache-dir -e .
 jupyter labextension develop . --overwrite
 
